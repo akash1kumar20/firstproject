88a20bcd5dc136abdc7531e5baaeb72745f501c9
akash1kumar20 <akashkumar2101sm@gmail.com>
2023-03-11 19:43:29 +0530

    Addition in js file

--- Project-1/main.js
+++ Project-1/main.js
@@ -5,6 +5,7 @@ console.log(document.title);
 var change2 = document.querySelector('body');
 change2.style.backgroundColor = "pink";
 var change3 = document.getElementById('header-title');
+console.log(typeof change3);
 change3.style.color='yellowgreen'
 change3.style.border='solid 5px #000'
 
