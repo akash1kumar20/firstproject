b832081edb6731a4255356f6e2a4d8f8c37d9c56
akash1kumar20 <akashkumar2101sm@gmail.com>
2023-03-11 15:30:34 +0530

    Dom file

---
+++ Project-1/main.js
@@ -0,0 +1,13 @@
+console.log(document.title);
+var change1 = document.querySelector('title');
+change1.textContent = 'Title changed'
+console.log(document.title);
+var change2 = document.querySelector('body');
+change2.style.backgroundColor = "pink";
+var change3 = document.getElementById('header-title');
+change3.style.color='yellowgreen'
+change3.style.border='solid 5px #000'
+
+var change4 = document.querySelectorAll('.title');
+change4[0].style.fontWeight = 'bold';
+change4[0].style.color = 'green'
