14b6af70ed6fe65e70caaadbe62b747034723ebb
akash1kumar20 <akashkumar2101sm@gmail.com>
2023-03-12 13:20:44 +0530

    parentNode

---
+++ creatingNodes/main.js
@@ -0,0 +1,6 @@
+// Traversing the Node
+
+var parent = document.querySelector('#items');
+//to select id use # before name
+parent.parentNode.style.backgroundColor = 'skyblue';
+//<div id="main" class="card card-body"> this is parent because the item we select is under this id.
